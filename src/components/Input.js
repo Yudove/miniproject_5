d2e484b970d5371185d2601cc37d4a1055d0d427
Yudove <105146326+Yudove@users.noreply.github.com>
2022-06-13 15:12:55 +0900

    input 스타일 수정

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,8 +3,14 @@ import styled from "styled-components";
 
 const StyledInput = styled.input`
 margin-bottom: 30px;
-width: 550px;
+margin-right: 10px;
+width: 540px;
 height: 19px;
+border-left-width:0; 
+border-right-width:0; 
+border-top-width:0; 
+border-bottom-width:1;
+
 `;
 
 const Input = () => {
